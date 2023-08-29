c5c470e55e405766d6cb7bd3c72a9af7e60fb680
Pupat3l <pujan.patel2002@gmail.com>
2023-08-29 09:59:10 -0400

    dynamic discussion

--- scripts.js
+++ scripts.js
@@ -1,38 +1,59 @@
-// Function to add a new discussion to the featured discussions array
-function addDiscussionToFeatured(title, content) {
-    const newDiscussion = {
-      id: featuredDiscussions.length + 1,
-      title: title,
-      content: content
-    };
-    featuredDiscussions.push(newDiscussion);
-  }
+const featuredDiscussions = [];
+
+// Get elements from the DOM
+const startFormButton = document.getElementById("start-form");
+const discussForm = document.getElementById("discuss-form");
+const discussTitleInput = document.getElementById("discuss-title");
+const discussContentInput = document.getElementById("discuss");
+const featuredDiscussionsSection = document.getElementById("featured-discussions");
+
+function addDiscussionToFeatured(title, content, timestamp) {
+  const newDiscussion = {
+    id: featuredDiscussions.length + 1,
+    title: title,
+    content: content,
+    timestamp: timestamp
+  };
+  featuredDiscussions.push(newDiscussion);
+}
+
+// Event listener for "Start a discussion" button
+startFormButton.addEventListener("click", function() {
+  discussForm.style.display = "block";
+  startFormButton.style.display = "none";
+});
+
+// Event listener for form submission
+discussForm.addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get discussion title and content
+  const discussionTitle = discussTitleInput.value;
+  const discussionContent = discussContentInput.value;
   
-  // Event listener for form submission
-  discussForm.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-  
-    // Get discussion title and content
-    const discussionTitle = document.getElementById("discuss-title").value;
-    const discussionContent = document.getElementById("discuss").value;
-  
-    // Add the new discussion to the featured discussions array
-    addDiscussionToFeatured(discussionTitle, discussionContent);
-  
-    // Create a new discussion card (similar to what you did before)
-    const discussionCard = document.createElement("div");
-    discussionCard.classList.add("discussion-card");
-    discussionCard.innerHTML = `
-      <h3>${discussionTitle}</h3>
-      <p>${discussionContent}</p>
-      <a href="#discussion/${featuredDiscussions.length}" class="btn">Read More</a>
-    `;
-  
-    // Append the new discussion card to the discussion list
-    discussionList.appendChild(discussionCard);
-  
-    // Clear form inputs
-    discussTitleInput.value = "";
-    discussContentInput.value = "";
-  });
-  
+  // Get current timestamp
+  const timestamp = new Date().toLocaleString();
+
+  // Add the new discussion to the featured discussions array
+  addDiscussionToFeatured(discussionTitle, discussionContent, timestamp);
+
+  // Create a new discussion card
+  const discussionCard = document.createElement("div");
+  discussionCard.classList.add("discussion-card");
+  discussionCard.innerHTML = `
+    <h3>${discussionTitle}</h3>
+    <p>${discussionContent}</p>
+    <p class="timestamp">${timestamp}</p>
+  `;
+
+  // Append the new discussion card to the featured discussions section
+  featuredDiscussionsSection.appendChild(discussionCard);
+
+  // Clear form inputs
+  discussTitleInput.value = "";
+  discussContentInput.value = "";
+
+  // Hide the form and show the "Start a discussion" button
+  discussForm.style.display = "none";
+  startFormButton.style.display = "block";
+});
